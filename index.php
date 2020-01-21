5fae4973d4730202461cc64c53fe8fe54c86bff7
Nebojsa Kozlovacki <nebojsa@Nebojsas-MacBook-Pro.local>
2020-01-21 12:37:32 +0100

    Add .htacces routing

--- index.php
+++ index.php
@@ -3,11 +3,24 @@
 include 'vendor/autoload.php';
 define('TEMPLATE_DIR','./view/');
 //$is_ajax = 'XMLHttpRequest' == ( $_SERVER['HTTP_X_REQUESTED_WITH'] ?? '' );
-$request = \Symfony\Component\HttpFoundation\Request::createFromGlobals();
+//$requestUri = \Symfony\Component\HttpFoundation\Request::createFromGlobals()->getRequestUri();
 
-echo 'Startscript <br>';
-//$controller = new Controller\Index($request);
+function request_path()
+{
+    $request_uri = explode('/', trim($_SERVER['REQUEST_URI'], '/'));
+    $script_name = explode('/', trim($_SERVER['SCRIPT_NAME'], '/'));
+    $parts = array_diff_assoc($request_uri, $script_name);
+    if (empty($parts))
+    {
+        return '/';
+    }
+    $path = implode('/', $parts);
+    if (($position = strpos($path, '?')) !== FALSE)
+    {
+        $path = substr($path, 0, $position);
+    }
+    return $path;
+}
+
 $factoryMethod = new \Controller\FactoryMethod\Controller();
-$factoryMethod->make("/");
-
-echo 'Endscript';
+$factoryMethod->make(request_path());
